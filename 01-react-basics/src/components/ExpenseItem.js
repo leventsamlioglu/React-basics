f5de38e62c9740330ec09bcd6b854148c271597a
leventsamlioglu <leventsamlioglu@icloud.com>
2021-12-23 09:03:20 +0100

    split components

--- 01-react-basics/src/components/ExpenseItem.js
+++ 01-react-basics/src/components/ExpenseItem.js
@@ -1,5 +1,5 @@
-import "./ExpenseItem.css";
-import ExpenseDate from "./ExpenseDate";
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
 
 function ExpenseItem(props) {
   return (
