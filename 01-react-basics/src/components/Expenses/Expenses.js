f9868c47e11c3d7238397af241905d395c1c2bcc
leventsamlioglu <leventsamlioglu@icloud.com>
2022-01-01 12:38:31 +0100

    add cancel and add expense button

--- 01-react-basics/src/components/Expenses/Expenses.js
+++ 01-react-basics/src/components/Expenses/Expenses.js
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
-import Card from "../UI/Card";
-import ExpensesFilter from "./ExpensesFilter";
-import "./Expenses.css";
-import ExpensesList from "./ExpensesList";
+import React, { useState } from 'react';
+
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+import './Expenses.css';
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState('2020');
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
