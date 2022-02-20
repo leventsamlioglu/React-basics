881414e1499f06f5ad693af73f3a3009d85bd8bc
leventsamlioglu <leventsamlioglu@icloud.com>
2022-02-20 12:00:44 +0100

    fetch data and add spinner

--- 05-food-order-app/src/components/Meals/AvailableMeals.js
+++ 05-food-order-app/src/components/Meals/AvailableMeals.js
@@ -1,37 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+import { css } from "@emotion/react";
+
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchedMeals = async () => {
+      const response = await fetch(
+        "https://react-app-91b20-default-rtdb.firebaseio.com/meals.json"
+      );
+      const data = await response.json();
+      const loadedMeals = [];
+
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+    fetchedMeals();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <ClipLoader
+        // color='#ffffff'
+        loading={isLoading}
+        css={override}
+        size={150}
+      />
+    );
+  }
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
