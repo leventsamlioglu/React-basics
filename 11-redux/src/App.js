4387abc71e5846907273f6466a1eafeb74d0b1f3
leventsamlioglu <leventsamlioglu@icloud.com>
2022-02-24 10:52:56 +0100

    multiple slices

--- 11-redux/src/App.js
+++ 11-redux/src/App.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Header from "../src/components/Header";
+import Auth from "../src/components/Auth";
 import Counter from "./components/Counter";
 
 function App() {
-  return <Counter />;
+  return (
+    <Fragment>
+      <Header />
+      <Auth />
+      <Counter />
+    </Fragment>
+  );
 }
 
 export default App;
