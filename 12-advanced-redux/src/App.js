b9d1911c2e8ec8719954260a88ad15458be81535
leventsamlioglu <leventsamlioglu@icloud.com>
2022-02-27 16:54:58 +0100

    useEffect with redux

--- 12-advanced-redux/src/App.js
+++ 12-advanced-redux/src/App.js
@@ -1,10 +1,19 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+
+  useEffect(() => {
+    fetch("https://react-app-91b20-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
   return (
     <Layout>
       {showCart && <Cart />}
