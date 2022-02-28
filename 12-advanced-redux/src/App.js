1fffb9f5b478a9856ddc96a8f6e3de42af4c2654
leventsamlioglu <leventsamlioglu@icloud.com>
2022-02-28 14:18:09 +0100

    get fetched data with redux

--- 12-advanced-redux/src/App.js
+++ 12-advanced-redux/src/App.js
@@ -3,23 +3,30 @@ import { useDispatch, useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { sendCartData } from "./components/store/cart-slice";
+import { sendCartData, fetchCartData } from "./components/store/cart-actions";
 import Notification from "./components/UI/Notification";
 
 let isInitial = true;
 
 function App() {
   const dispatch = useDispatch();
-  const cart = useSelector((state) => state.cart);
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
 
   useEffect(() => {
     if (isInitial) {
       isInitial = false;
       return;
     }
-    dispatch(sendCartData(cart));
+
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
 
   return (
--- 12-advanced-redux/src/components/Cart/Cart.js
+++ 12-advanced-redux/src/components/Cart/Cart.js
@@ -5,6 +5,7 @@ import CartItem from "./CartItem";
 
 const Cart = (props) => {
   const cartItems = useSelector((state) => state.cart.items);
+
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
@@ -14,7 +15,7 @@ const Cart = (props) => {
             key={item.id}
             item={{
               id: item.id,
-              title: item.title,
+              title: item.name,
               quantity: item.quantity,
               total: item.totalPrice,
               price: item.price,
