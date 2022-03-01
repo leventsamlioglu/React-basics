b86023ff7ace5ebd202464a50cd05c772a43b67e
leventsamlioglu <leventsamlioglu@icloud.com>
2022-03-01 12:43:52 +0100

    define routes

--- 13-react-router/src/App.js
+++ 13-react-router/src/App.js
@@ -1,7 +1,15 @@
+import { Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Welcome from "./pages/Welcome";
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Route path='/welcome'>
+        <Welcome />
+      </Route>
+      <Route path='/product'>
+        <Product />
+      </Route>
     </div>
   );
 }
