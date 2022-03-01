7f53aba70c1137c3971522c8b56101dd6f6c7399
leventsamlioglu <leventsamlioglu@icloud.com>
2022-03-01 14:08:53 +0100

    extracting rouute params

--- 13-react-router/src/pages/Products.js
+++ 13-react-router/src/pages/Products.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Products = () => {
-  return <h1>Product page!</h1>;
+  return (
+    <section>
+      <h1>Products page!</h1>
+      <ul>
+        <li>A Book</li>
+        <li>A Carpet</li>
+        <li>An Online Course</li>
+      </ul>
+    </section>
+  );
 };
 
 export default Products;
