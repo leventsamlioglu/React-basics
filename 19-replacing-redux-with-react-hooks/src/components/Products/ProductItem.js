3bae37576cdc23b5fd5a278f7675897bb400d6be
leventsamlioglu <leventsamlioglu@icloud.com>
2022-03-15 10:23:46 +0100

    finish project

--- 19-replacing-redux-with-react-hooks/src/components/Products/ProductItem.js
+++ 19-replacing-redux-with-react-hooks/src/components/Products/ProductItem.js
@@ -1,29 +1,32 @@
-import React, { useContext } from "react";
-import Card from "../UI/Card";
-import { ProductsContext } from "../../context/products-context";
-import "./ProductItem.css";
+import React from 'react';
 
-const ProductItem = (props) => {
-  const toggleFav = useContext(ProductsContext).toggleFav;
+import Card from '../UI/Card';
+import { useStore } from '../../hooks-store/store';
+import './ProductItem.css';
+
+const ProductItem = React.memo(props => {
+  console.log('RENDERING');
+  const dispatch = useStore(false)[1];
 
   const toggleFavHandler = () => {
-    toggleFav(props.id);
+    // toggleFav(props.id);
+    dispatch('TOGGLE_FAV', props.id);
   };
 
   return (
-    <Card style={{ marginBottom: "1rem" }}>
-      <div className='product-item'>
-        <h2 className={props.isFav ? "is-fav" : ""}>{props.title}</h2>
+    <Card style={{ marginBottom: '1rem' }}>
+      <div className="product-item">
+        <h2 className={props.isFav ? 'is-fav' : ''}>{props.title}</h2>
         <p>{props.description}</p>
         <button
-          className={!props.isFav ? "button-outline" : ""}
+          className={!props.isFav ? 'button-outline' : ''}
           onClick={toggleFavHandler}
         >
-          {props.isFav ? "Un-Favorite" : "Favorite"}
+          {props.isFav ? 'Un-Favorite' : 'Favorite'}
         </button>
       </div>
     </Card>
   );
-};
+});
 
 export default ProductItem;
--- 19-replacing-redux-with-react-hooks/src/index.js
+++ 19-replacing-redux-with-react-hooks/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import ProductsProvider from "./context/products-context";
+import configureProductsStore from "./hooks-store/products-store";
+
+configureProductsStore();
 
 ReactDOM.render(
-  <ProductsProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ProductsProvider>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById("root")
 );
